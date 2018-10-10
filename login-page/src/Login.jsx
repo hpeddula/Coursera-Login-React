9fbef0ed93d28489732fe548b998f472e2edda44
Harsha <hpeddula@outlook.com>
2018-10-10 13:04:29 +0530

    Awesome Progress

--- login-page/src/Login.jsx
+++ login-page/src/Login.jsx
@@ -7,15 +7,15 @@ export default class Login extends React.Component {
     }
     render() {
         const tooltip=
-        `<div>
-            <h2 className="text-danger">Trying to load Htm through tooltip</h2>
+        `<div class="login-tooltip">
+            <h2 class="text-danger">Trying to load Html through tooltip</h2>
         </div>`
         return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-4 col-lg-4"></div>
                     <div className="col-sm-4 col-lg-4 text-center login-box">
-                        <button data-toggle="tooltip" data-placement="bottom" data-trigger="focus" data-html="true"  title={tooltip.toString()} className="btn btn-success sign-in">Login</button>
+                        <button data-toggle="tooltip" data-placement="bottom" data-trigger="hover" data-html="true"  title={tooltip.toString()} className="btn btn-success sign-in">Login</button>
                         <button data-toggle="tooltip" data-placement="bottom" title="Sign-Up" className="btn btn-primary sign-up">Sign Up</button>
                     </div>
                     <div className="col-sm-4 col-lg-4"></div>
