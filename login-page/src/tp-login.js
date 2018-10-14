c21bdb09bf3a373a1bedd14abd3d71d1a997ff25
Harsha <hpeddula@outlook.com>
2018-10-14 21:07:17 +0530

    Adding changes to tp-login

--- login-page/src/tp-login.js
+++ login-page/src/tp-login.js
@@ -23,38 +23,40 @@ export default class TpLogin extends React.Component {
     }
     render() {
         return (
-            <div className="container-fluid p-login" style={{ background: 'url(https://www.tpx.com/wp-content/uploads/2017/03/TPx-Complete-Slider-Image-July-9-Fits.jpg)' }}>
-                <div className="row">
-                    <div className="col-sm-4 col-lg-4">
-                        <div className="card sha">
-                            <div className="card-header text-center">
-                                <h4 className="tp-font">TPX - Login</h4>
-                            </div>
-                            <div className="card-body">
-                                <form>
-                                    <div className="form-group" htmlFor="email">
-                                        <TextField id="email" label="Email" margin="normal" fullWidth={true} placeholder="Email" inputProps={{
-                                            ref: (node) => { this.email= node; },
-                                                            }} 
-                                            helperText="Enter an Email" error={true}   />
-                                    </div>
-                                    <div className="form-group" htmlFor="pwd">
-                                        <TextField id="pwd" label="Password" type="password" fullWidth={true} inputProps={{
-                                            ref: (node) => { this.pwd= node; },
-                                        }} autoComplete="current-password" margin="normal" />
-                                    </div>
-                                </form>
-                            </div>
-                            <div className="card-footer">
-                                <button className="btn btns btn-block" onClick={this.pdf}> Sign-In</button>
-                                <button className="btn btns btn-block" onClick={this.redirectToWalter}> Sign-Up</button>
-                                <input className="btn btn-primary btn-block" onClick={this.handleSubmit} type="submit" value="Submit" />
+            <div className="p-login">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-4">
+                            <div className="card sha">
+                                <div className="card-header text-center">
+                                    <h4 className="tp-font">TPX - Login</h4>
+                                </div>
+                                <div className="card-body">
+                                    <form>
+                                        <div className="form-group" htmlFor="email">
+                                            <TextField id="email" label="Email" margin="normal" fullWidth={true} placeholder="Email" inputProps={{
+                                                ref: (node) => { this.email = node; },
+                                            }}
+                                                helperText="Enter an Email" error={true} />
+                                        </div>
+                                        <div className="form-group" htmlFor="pwd">
+                                            <TextField id="pwd" label="Password" type="password" fullWidth={true} inputProps={{
+                                                ref: (node) => { this.pwd = node; },
+                                            }} autoComplete="current-password" margin="normal" />
+                                        </div>
+                                    </form>
+                                </div>
+                                <div className="card-footer">
+                                    <button className="btn btns btn-block" onClick={this.pdf}> Sign-In</button>
+                                    <button className="btn btns btn-block" onClick={this.redirectToWalter}> Sign-Up</button>
+                                    <input className="btn btn-primary btn-block" onClick={this.handleSubmit} type="submit" value="Submit" />
+                                </div>
                             </div>
                         </div>
+                        <div className="col-4">
+                        </div>
+                        <div className="col-4"></div>
                     </div>
-                    <div className="col-sm-4 col-lg-4">
-                    </div>
-                    <div className="col-sm-4 col-lg-4"></div>
                 </div>
             </div>
         );
